1da6483b7f2b29c5d39ec8880d16d52de0947fb3
Ryan <error.nointernet@gmail.com>
2021-07-17 14:38:56 +0800

    Added buffers for stdin

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
@@ -11,12 +10,17 @@ func main() {
 	var length int
 
 	if len(os.Args) == 1 {
-		data, errorObject := ioutil.ReadAll(os.Stdin)
-		if errorObject != nil {
-			fmt.Println(errorObject)
-			os.Exit(1)
+		buffer := make([]byte, 8000000)
+		for {
+			size, errorObject := os.Stdin.Read(buffer)
+			length += size
+			if errorObject == io.EOF {
+				break
+			} else if errorObject != nil {
+				fmt.Println(errorObject)
+				os.Exit(1)
+			}
 		}
-		length += len(data)
 	} else {
 		os.Args = os.Args[1:]
 		for _, argument := range os.Args {
